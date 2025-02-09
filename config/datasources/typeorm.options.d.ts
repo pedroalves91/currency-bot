7a7da616cee5fc8c4d21016fe982cea0d06b6e71
Pedro <alvespedro991@gmail.com>
2025-02-09 22:36:37 +0000

    remove unused files

--- config/datasources/typeorm.options.d.ts
+++
@@ -1,7 +0,0 @@
-import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-export declare class TypeOrmOptions implements TypeOrmOptionsFactory {
-    private readonly configService;
-    constructor(configService: ConfigService);
-    createTypeOrmOptions(): TypeOrmModuleOptions;
-}
